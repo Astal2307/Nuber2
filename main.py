fcb9e981e9fd0510a1d639bc37a8a84c9f7a62b8
Astal2307 <151533154+Astal2307@users.noreply.github.com>
2023-11-21 22:31:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,58 +9,11 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt, QPoint
 
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>20</y>
-      <width>111</width>
-      <height>61</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Create circle</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>20</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>"""
-
-
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        abc = io.StringIO(template)
-        uic.loadUi(abc, self)
+        # abc = io.StringIO(template)
+        uic.loadUi('UI.ui, self)
         self.initUI()
 
     def initUI(self):
